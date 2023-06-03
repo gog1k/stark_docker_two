2e4904315e6c7cfc7ba906c81f34aa7cd9c6afda
Yuri Merkury <yg@trueflip.io>
2023-06-03 12:40:46 +0300

    init

--- resources/achievements_back/dc_command.sh
+++ resources/achievements_back/dc_command.sh
@@ -4,13 +4,13 @@ ln -sf /resources/achievements_back/.env /data/achievements_back/.env
 
 bash /resources/wait-for-mysql.sh
 
-if [[ $1 = 'build' ]]; then
+#if [[ $1 = 'build' ]]; then
 
   cd /data/achievements_back
   sudo -u docker composer install
   sudo -u docker composer update
 
-fi
+#fi
 
 php /data/achievements_back/artisan migrate
 php-fpm
--- resources/achievements_front/dc_command.sh
+++ resources/achievements_front/dc_command.sh
@@ -2,7 +2,7 @@
 
 ln -sf /resources/achievements_front/.env /data/achievements_front/.env
 
-if [[ $1 = 'build' ]]; then
+#if [[ $1 = 'build' ]]; then
   sudo -u docker npm cache clean --force
   sudo -u docker npm config set fetch-retry-mintimeout 20000
   sudo -u docker npm config set fetch-retry-maxtimeout 120000
@@ -11,7 +11,7 @@ if [[ $1 = 'build' ]]; then
   cd /data/achievements_front
   rm -rf node_modules
   sudo -u docker npm install
-fi
+#fi
 
 cd /data/achievements_front
 sudo -u docker npm run serve
